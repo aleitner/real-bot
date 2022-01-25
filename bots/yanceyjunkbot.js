aae8cdb7c89dc3342caab9437a7df93212b3d0c5
aleitner <leitnersalex@gmail.com>
2022-01-24 19:03:57 -0500

    testing

--- bots/yanceyjunkbot.js
+++ bots/yanceyjunkbot.js
@@ -9,20 +9,11 @@ class YanceyJunkBot {
             // return
         }
 
-        msg.channel.awaitMessages(filter, {
-            max: 1,
-            time: 30000,
-            errors: ['time']
-          })
-          // not the same; from awaitMessages() instead
-          .then(originalMsg => {
-            originalMsg = originalMsg.first()
-            var newMessage = originalMsg.content.replace(/[^A-Z0-9]+/ig, " ");
+        const repliedTo = await msg.channel.messages.fetch(message.reference.messageID);
+        console.log(repliedTo.content)
+        var newMessage = repliedTo.content.replace(/[^A-Z0-9]+/ig, " ");
 
-            console.log(newMessage)
-
-          })
-        // msg.reply(`What <@${msg.author.id}> might be trying to say is \n"${newMessage}".`)
+        // msg.channel.send(`What <@${msg.author.id}> might be trying to say is \n"${newMessage}".`)
     }
 }
 
